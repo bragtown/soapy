b14f9608b0ac2f6e5140760a0f9927cf9db09044
Jordan Braginton <jordan.braginton@gmail.com>
2014-03-13 20:45:21 -0600

    added visit capability, working on push to visit capability

--- app/scripts/controllers/checkin.js
+++ app/scripts/controllers/checkin.js
@@ -2,54 +2,68 @@
 
 angular.module('soapyApp')
   .controller('CheckinCtrl', function ($scope, $firebase) {
-  	var patientRef = new Firebase('https://soapnotes.firebaseIO.com/patients');
+  	var patientRef = new Firebase('https://soapnotes.firebaseIO.com/patients/');
   	$scope.patients = $firebase(patientRef);
   	$scope.patient = {
   		name: "",
-      date: ""
+      date: "",
+      timeIn:""
   	};
   	$scope.addPatient = function(e){
     	if(e.keyCode !== 13) {
     		return;
     	};
-      var boolName;
       var patientList = $scope.patients.$getIndex();
-      for (var i = 0; i < patientList.getLength && boolName; i ++){
-        var patientId = new Firebase('https://soapnotes.firebaseIO.com/patients/'+patientList[i]);
+      for (var patient in patientList){
+        var patientId = new Firebase('https://soapnotes.firebaseIO.com/patients/'+patientList[patient]);
         patientId.on('value', function (snapshot){
-          boolName = snapshot.val().body === $scope.patient.name;
-          console.log(boolName);
+          var boolName = snapshot.val().body === $scope.patient.name;
           if (boolName){
-            return;
+            $scope.addName = true;
+            $scope.currentPatient = patientList[patient];
           };
         });
       };
-    	if (!boolName){
-        console.log('in checkname checker');
+    	if (!$scope.addName){
       	$scope.patients.$add({
       		body: $scope.patient.name
       	});
+        patientList = $scope.patients.$getIndex();
+        $scope.currentPatient = patientList[(patientList.length-1)];
       };
     };
     $scope.addDate = function(e){
       if(e.keyCode !== 13) {
         return;
       };
-      //get firebase id for my current scope.patient.name
-      var currentPatient;
-      var patientList = $scope.patients.$getIndex();
-      for (var patient in patientList){
-        var patientId = new Firebase('https://soapnotes.firebaseIO.com/patients/'+patientList[patient]);
-        patientId.on('value', function (snapshot){
-          if(snapshot.val().body === $scope.patient.name){
-            currentPatient = patientId;
-          }
-        });
+      if ($scope.currentPatient === undefined){
+        alert('Please ENTER patient name')
+        return;
       }
-
-      $scope.thisPatient = $firebase(patientId);
-      $scope.thisPatient.$add({
-        body: $scope.patient.date
-      });
+      var patientId = $scope.currentPatient;
+      $scope.passId = patientId;
+      var currentRef = new Firebase('https://soapnotes.firebaseIO.com/patients/'+patientId);
+      $scope.passCurrentRef = currentRef;
+      $scope.currentPatient = $firebase(currentRef);
+      $scope.currentPatient.$add({
+        visit:$scope.patient.date
+      }); 
     };
+    $scope.addTimeIn = function(e){
+     if(e.keyCode !== 13) {
+        return;
+      };
+      $scope.passCurrentRef.on('value', function (snapshot){
+          $scope.currentVisit = snapshot.exportVal();
+        }); 
+      var patientVisit = $scope.currentVisit;
+      console.log(patientVisit);
+      var patientId = $scope.passId;
+      console.log(patientId);
+      var visitRef = new Firebase('https://soapnotes.firebaseIO.com/patients/'+patientId+'/'+patientVisit);  
+      $scope.patientVisit = $firebase(visitRef);
+      $scope.patientVisit.$add({
+        timeIn:$scope.patient.timeIn
+      });
+    }
 });
